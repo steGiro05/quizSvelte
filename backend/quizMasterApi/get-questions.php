0d6210414f6123a2abfcfba79c1be8a53dc709d0
steGiro05 <137765255+steGiro05@users.noreply.github.com>
2024-01-27 10:28:33 +0100

    Add files via upload

---
+++ backend/quizMasterApi/get-questions.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin:http://localhost:5173");
+header('Content-Type: application/json');
+require_once(__DIR__ . '/protected/config.php');
+
+
+
+try {
+    $query = $db->prepare('SELECT * from quiz where id= :id');
+    $query->bindParam('id', $_GET['id'], PDO::PARAM_INT);
+    $query->execute();
+    $rows = $query->fetchAll();
+    echo '{"status":1, "data":' . json_encode($rows) . '}';
+} catch (PDOException $ex) {
+    sendError('error executing query', __LINE__);
+}
+
+
+function sendError($message = 'error', $debug = 0)
+{
+    echo '{"status":0, "message":"' . $message . '", "debug": ' . $debug . '}';
+    exit();
+}
